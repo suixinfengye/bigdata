9f6645c381af7dd0303c2aa6ed32031a2e14faca
suixinfengye <1430657824@qq.com>
2018-09-29 01:18:49 +0800

    hbase update

--- src/main/scala/sample/HbaseSpark.scala
+++ src/main/scala/sample/HbaseSpark.scala
@@ -1,9 +1,10 @@
 package sample
 
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.client.{HTable, Put}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
-import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
+import org.apache.hadoop.hbase.mapred.TableOutputFormat
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.mapred.JobConf
 import org.apache.spark.internal.Logging
@@ -20,7 +21,7 @@ import org.apache.spark.util.StatCounter
 //--executor-memory 700m \
 //--total-executor-cores 2 \
 ///usr/local/userlib/jars/bigdata.jar
-
+//https://www.jianshu.com/p/0db275b06496
 object HbaseSpark extends Logging {
   def main(args: Array[String]) {
     val spark = SparkSession
@@ -29,35 +30,38 @@ object HbaseSpark extends Logging {
       .appName("HbaseSpark")
       .getOrCreate()
 
-  }
-
-  def readHbase(spark: SparkSession): Unit = {
+    val tableName = "test"
     val conf = HBaseConfiguration.create()
     conf.set("hbase.zookeeper.property.clientPort", "2181"); //设置zookeeper client端口
     conf.set("hbase.zookeeper.quorum", "spark1,spark2,spark3")
-    conf.addResource("/usr/local/userlib/hbase/conf/hbase-site.xml") //将hbase的配置加载
-    conf.set(org.apache.hadoop.hbase.mapreduce.TableInputFormat.INPUT_TABLE, "test")
+    //    conf.addResource("/usr/local/userlib/hbase/conf/hbase-site.xml") //将hbase的配置加载
+    conf.set(org.apache.hadoop.hbase.mapreduce.TableInputFormat.INPUT_TABLE, tableName)
+
+    readHbase(spark,conf)
+    writeHbase(spark,conf)
+  }
+
+  def readHbase(spark: SparkSession,conf: Configuration): Unit = {
     val hbaseRDD = spark.sparkContext.newAPIHadoopRDD(conf, classOf[org.apache.hadoop.hbase.mapreduce.TableInputFormat], classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable], classOf[org.apache.hadoop.hbase.client.Result])
     val value: Long = hbaseRDD.count()
     logInfo("---------------------------============================----------------------------------: " + value)
-    logError("---------------------------============================----------------------------------: " + value)
+//    /
   }
 
-  def writeHbase(spark:SparkSession):Unit ={
-    val conf = HBaseConfiguration.create()
-    val jobConfig: JobConf = new JobConf(conf, this.getClass)
-    jobConfig.set("mapreduce.output.fileoutputformat.outputdir", "/user/user01/out")
-//    jobConfig.setOutputFormat(classOf[TableOutputFormat])
-    jobConfig.set(TableOutputFormat.OUTPUT_TABLE, "test")
-    // convert rowkey, psi stats to put and write to hbase table stats column family
-//    keyStatsRDD.map { case (k, v) => convertToPut(k, v) }.saveAsHadoopDataset(jobConfig)
-    convertToPut().save
+  def writeHbase(spark:SparkSession,conf: Configuration):Unit ={
+    val jobConf = new JobConf(conf,this.getClass)
+    jobConf.setOutputFormat(classOf[TableOutputFormat])
+    jobConf.set(TableOutputFormat.OUTPUT_TABLE,"test")
+    val rawData = List(("03","info","name","haha"))
+    val localData = spark.sparkContext.parallelize(rawData).map(t=>converToPut(HbaseCell(t._1,t._2,t._3,t._4)))
+    localData.saveAsHadoopDataset(jobConf)
   }
-  // convert rowkey, stats to put
-  def convertToPut(): (ImmutableBytesWritable, Put) = {
-    val p = new Put(Bytes.toBytes("key"))
-    // add columns with data values to put
-    p.add(Bytes.toBytes("cloumn famliy"), Bytes.toBytes("cloumn qulify"), Bytes.toBytes("dddd"))
-    (new ImmutableBytesWritable, p)
+
+  def converToPut(c:HbaseCell) ={
+    val put = new Put(Bytes.toBytes(c.rowKey))
+    put.addColumn(Bytes.toBytes(c.cf),Bytes.toBytes(c.qf),Bytes.toBytes(c.value))
+    (new ImmutableBytesWritable, put)
   }
+
 }
+case class HbaseCell(rowKey:String,cf:String,qf:String,value:String)
