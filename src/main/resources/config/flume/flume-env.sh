633403db7d9f7b5eafad089fb8bfd99d02ed43b4
suixinfengye <1430657824@qq.com>
2018-11-14 01:11:33 +0800

    fulme jvm tuning

---
+++ src/main/resources/config/flume/flume-env.sh
@@ -0,0 +1,40 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# If this file is placed at FLUME_CONF_DIR/flume-env.sh, it will be sourced
+# during Flume startup.
+
+# Enviroment variables can be set here.
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+# Give Flume more memory and pre-allocate, enable remote monitoring via JMX
+# 默认20m
+# export JAVA_OPTS="-Xms100m -Xmx200m -Dcom.sun.management.jmxremote"
+# 支持JMX监控,也可以用Jconsole监控
+export JAVA_OPTS="-server -Xms200m -Xmx500m -XX:+PrintFlagsFinal -XX:+PrintGCDetails -Xloggc:/home/feng/software/flume/logs/fulme_gc.log -XX:+PrintGCTimeStamps 
+-XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution -XX:NewRatio=1 
+-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=5445 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
+-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/feng/software/data/flume_heapdump.hprof"
+
+# Let Flume write raw event data and configuration information to its log files for debugging
+# purposes. Enabling these flags is not recommended in production,
+# as it may result in logging sensitive user information or encryption secrets.
+# export JAVA_OPTS="$JAVA_OPTS -Dorg.apache.flume.log.rawdata=true -Dorg.apache.flume.log.printconfig=true "
+
+# Note that the Flume conf directory is always included in the classpath.
+#FLUME_CLASSPATH=""
+
