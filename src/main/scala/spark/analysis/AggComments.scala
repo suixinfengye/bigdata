1da91ae87b339d7144ae5bd34d2d1b1e6e6336d1
suixinfengye <1430657824@qq.com>
2019-01-20 23:52:13 +0800

    update

--- src/main/scala/spark/analysis/AggComments.scala
+++ src/main/scala/spark/analysis/AggComments.scala
@@ -2,44 +2,53 @@ package spark.analysis
 
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-import org.apache.spark.sql.types.{DataType, StructField, StructType}
-import org.joni.constants.StringType
+import org.apache.spark.sql.types.{DataType, StringType, StructField, StructType}
 
 /**
   * 给电影评论做聚合,将多条评论聚合成一个string
   * feng
   * 19-1-19
   */
-class AggComments extends UserDefinedAggregateFunction{
+class AggComments extends UserDefinedAggregateFunction {
+
   // inputSchema，输入数据的类型
   def inputSchema: StructType = {
-    StructType(Array(StructField("str", StringType, true)))
+    StructType(Array(StructField("COMMENT", StringType, true)))
   }
+
   // bufferSchema，中间进行聚合时，所处理的数据的类型
   def bufferSchema: StructType = {
-    StructType(Array(StructField("comments", StringType, true)))
+    StructType(Array(StructField("content", StringType, true)))
   }
+
   // dataType，函数返回值的类型
   def dataType: DataType = {
     StringType
   }
+
+  //Deterministic just means that for the same input the function will return the same output.
+  //Unless there is some randomness in your function it'll always be deterministic.
   def deterministic: Boolean = {
     true
   }
+
   // 为每个分组的数据执行初始化操作
   def initialize(buffer: MutableAggregationBuffer): Unit = {
     buffer(0) = ""
   }
+
   // 由于Spark是分布式的，所以一个分组的数据，可能会在不同的节点上进行局部聚合，就是update
   // 每个分组，有新的值进来的时候，如何进行分组对应的聚合值的计算
   def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
     buffer(0) = buffer.getAs[String](0) + input.getAs[String](0)
   }
+
   // 最后一个分组，在各个节点上的聚合值，要进行merge，也就是合并
   def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
     buffer1(0) = buffer1.getAs[String](0) + buffer2.getAs[String](0)
   }
-  // 最后，一个分组的聚合值，如何通过中间的缓存聚合值，最后返回一个最终的聚合值
+
+  // 返回一个最终的聚合值
   def evaluate(buffer: Row): Any = {
     buffer.getAs[String](0)
   }
--- src/main/scala/spark/analysis/MovieKeyWords.scala
+++ src/main/scala/spark/analysis/MovieKeyWords.scala
@@ -7,7 +7,7 @@ import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.internal.Logging
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import spark.dataProcess.ProcessMysqlData.logInfo
 import spark.dto.Review
@@ -15,16 +15,16 @@ import utils.{AnsjUtils, CommonUtil, MysqlUtil}
 
 /**
   * https://my.oschina.net/uchihamadara/blog/2032481
-  * ./bin/spark-submit \
-  * --class spark.analysis.MovieKeyWords \
-  * --master spark://spark1:7077 \
-  * --executor-memory 1G \
-  * --num-executors 2 \
-  * --total-executor-cores 2 \
-  * --files "/usr/local/userlib/spark-2.2/conf/log4j-executor.properties" \
-  * --driver-java-options "-Dlog4j.debug=true -Dlog4j.configuration=log4j.properties" \
-  * --conf "spark.executor.extraJavaOptions=-Dlog4j.debug=true -Dlog4j.configuration=log4j-executor.properties -XX:+PrintGCDetails -Xloggc:/usr/local/userlib/spark-2.2/logs/executor_gc.log -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+UseG1GC -XX:+PrintTenuringDistribution -Xms400m -XX:+PrintCommandLineFlags -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/usr/local/userlib/spark-2.2/logs/executor_oom.hprof" \
-  * /usr/local/userlib/jars/bigdata.jar
+./bin/spark-submit \
+--class spark.analysis.MovieKeyWords \
+--master spark://spark1:7077 \
+--executor-memory 1G \
+--num-executors 3 \
+--total-executor-cores 3 \
+--files "/usr/local/userlib/spark-2.2/conf/log4j-executor.properties" \
+--driver-java-options "-Dlog4j.debug=true -Dlog4j.configuration=log4j.properties" \
+--conf "spark.executor.extraJavaOptions=-Dlog4j.debug=true -Dlog4j.configuration=log4j-executor.properties -XX:+PrintGCDetails -Xloggc:/usr/local/userlib/spark-2.2/logs/executor_gc.log -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+UseG1GC -XX:+PrintTenuringDistribution -Xms400m -XX:+PrintCommandLineFlags -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/usr/local/userlib/spark-2.2/logs/executor_oom.hprof" \
+/usr/local/userlib/jars/bigdata.jar
   * feng
   * 19-1-10
   */
@@ -36,13 +36,23 @@ object MovieKeyWords extends Logging {
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .getOrCreate()
 
-    computeMovieKeyWords(spark)
-    readComment(spark)
+    val reviewDF = readReview(spark)
+    val commentDF = readComment(spark)
+
+    computeMovieKeyWords(spark, reviewDF)
+    computeMovieKeyWords(spark, commentDF)
+    val unionDF = reviewDF.join(commentDF, "movieid")
+    computeMovieKeyWords(spark, unionDF)
 
     spark.stop()
   }
 
-  def computeMovieKeyWords(spark: SparkSession): Unit = {
+  /**
+    * 读取 影评
+    *
+    * @param spark
+    */
+  def readReview(spark: SparkSession): DataFrame = {
     import spark.implicits._
     val conf = CommonUtil.getReadHbaseConfig("review")
     val hbaseRDD = spark.sparkContext.newAPIHadoopRDD(conf, classOf[TableInputFormat], classOf[ImmutableBytesWritable], classOf[Result])
@@ -63,20 +73,11 @@ object MovieKeyWords extends Logging {
       case (movieid: String, contents: String) =>
         Row(movieid, contents)
     }
-    val schema = StructType(Array(StructField("moiveid", StringType, false), StructField("content", StringType, false)))
+    val schema = StructType(Array(StructField("movieid", StringType, false), StructField("content", StringType, false)))
     val reveiwDF = spark.createDataFrame(movieid2ReviewsRDD, schema)
-
-    val ansj = new AnsjUtils
-    val bansj = spark.sparkContext.broadcast(ansj)
-    val convert = reveiwDF.map {
-      case Row(movieid: String, content: String) => {
-        val ansjInstance = bansj.value
-        val keyWords = ansjInstance.getKeywords(content).toString
-        (movieid, keyWords)
-      }
-    }
-    convert.printSchema()
-    convert.take(10).foreach(print(_))
+    reveiwDF.printSchema()
+    reveiwDF.take(10).foreach(print(_))
+    reveiwDF
   }
 
   /**
@@ -86,17 +87,30 @@ object MovieKeyWords extends Logging {
     * @return
     */
   def readComment(spark: SparkSession): DataFrame = {
+    import spark.implicits._
     val commentDF = CommonUtil.readPhoenixTable(spark, "movie_essay")
-//    commentDF.printSchema()
-//    commentDF.show()
-
-//    commentDF.select("MOVIEID","COMMENT").groupBy("MOVIEID").agg("MOVIEID",)
-//    commentDF
-
     commentDF.createOrReplaceTempView("essaytmp")
-    spark.sql("select MOVIEID,aggComments(COMMENT) from essaytmp group by MOVIEID")
+    //     定义和注册自定义函数
+    spark.udf.register("aggComments", new AggComments)
+    val movieCommentDF: DataFrame = spark.sql("select MOVIEID as movieid, aggComments(COMMENT) as content from essaytmp " +
+      "group by MOVIEID")
+    movieCommentDF.take(10).foreach(print(_))
+    movieCommentDF.show()
+    movieCommentDF
+  }
 
-
-
+  def computeMovieKeyWords(spark: SparkSession, df: DataFrame): Unit = {
+    import spark.implicits._
+    val ansj = new AnsjUtils
+    val bansj = spark.sparkContext.broadcast(ansj)
+    val convert = df.map {
+      case Row(movieid: String, content: String) => {
+        val ansjInstance = bansj.value
+        val keyWords = ansjInstance.getKeywords(content).toString
+        (movieid, keyWords)
+      }
+    }
+    convert.printSchema()
+    convert.take(10).foreach(print(_))
   }
 }
